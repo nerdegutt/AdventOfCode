7f268c054dd0a5793d35a925bdb23f857a30fcb6
Erlend Schei <erlend@erlend.com>
2022-12-07 17:50:59 +0100

    Finally fixed Day 7

--- 7/runme.js
+++ 7/runme.js
@@ -1,6 +1,29 @@
 const { input } = require('./input.js');
 
 const history = input.toString().trim().split('\n');
+/* const history = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`.split('\n'); */
 
 var stack = [];
 var fs = new Map();
@@ -37,10 +60,33 @@ while ((upNext = history.shift())) {
   }
 }
 
-const partOne = fs;
+var totalMax100k = 0;
+var allDirs = [];
+
+function calculateSize(name, dir) {
+  let currentSize = 0;
+  dir.forEach((value, key) => {
+    if (typeof value === 'object') {
+      currentSize += calculateSize(key, value);
+    } else {
+      currentSize += value;
+    }
+  });
+  //console.log(name, currentSize);
+  if (currentSize <= 100_000) totalMax100k += currentSize;
+  allDirs.push(currentSize);
+  return currentSize;
+}
+
+const usedSpace = calculateSize('/', fs.get('/'));
+
+const partOne = totalMax100k;
 console.log(partOne);
 
 //------------------------------
 
-const partTwo = partOne;
-//console.log(partTwo);
+const freeSpace = 70_000_000 - usedSpace;
+const minimumToDelete = 30_000_000 - freeSpace;
+
+const partTwo = allDirs.filter((val) => val > minimumToDelete).sort((a, b) => a - b)[0];
+console.log(partTwo);
