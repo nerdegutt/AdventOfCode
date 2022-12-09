b026990beee0a80d728500b18265b502b5074168
Erlend Schei <erlend@erlend.com>
2022-12-09 14:50:33 +0100

    Day 9 part one solved

--- 2022/9/runme.js
+++ 2022/9/runme.js
@@ -1,8 +1,8 @@
 const { input } = require('./input.js');
 
-//const moves = input.toString().trim().split('\n');
+const moves = input.toString().trim().split('\n');
 
-//*//
+/*//
 const moves = `
 R 4
 U 4
@@ -17,7 +17,62 @@ R 2
   .split('\n');
 //**/
 
-const partOne = moves;
+let hPosX = 0,
+  hPosY = 0,
+  tPosX = 0,
+  tPosY = 0,
+  hPos = [hPosX, hPosY],
+  tPos = [tPosX, tPosY],
+  tStack = new Set();
+
+function getNextTailPosition([hx, hy], [tx, ty]) {
+  let dx = hx - tx,
+    dy = hy - ty;
+  //console.log({ dx, dy });
+  if (dx > 1) {
+    // moved right
+    return [++tx, (ty += dy)];
+  } else if (dx < -1) {
+    // moved left
+    return [--tx, (ty += dy)];
+  } else if (dy > 1) {
+    // moved up
+    return [(tx += dx), ++ty];
+  } else if (dy < -1) {
+    // moved down
+    return [(tx += dx), --ty];
+  }
+  return [tx, ty];
+}
+
+moves.forEach((move, i) => {
+  let [direction, length] = move.split(' ');
+  length = parseInt(length);
+
+  for (let i = 0; i < length; i++) {
+    //console.log({ hPos, direction });
+    switch (direction) {
+      case 'R':
+        hPos = [++hPosX, hPosY];
+        break;
+      case 'L':
+        hPos = [--hPosX, hPosY];
+        break;
+      case 'U':
+        hPos = [hPosX, ++hPosY];
+        break;
+      case 'D':
+        hPos = [hPosX, --hPosY];
+        break;
+    }
+    tPos = getNextTailPosition(hPos, tPos);
+    tStack.add(tPos.join('-')); // Equal arrays in set will be considered different
+
+    //console.log({ hPos, tPos });
+  }
+});
+
+const partOne = tStack.size;
 console.log('Part one: ', partOne);
 
 //------------------------------
